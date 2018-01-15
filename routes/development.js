227d8ce4c1023889be026aee5c994ad8bf648803
Martin Bock <mail@mnbo.de>
2018-01-15 16:39:15 +0100

    partly implement seed/purge

---
+++ routes/development.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const User = require('../models/user');
+
+router.get('/seed-database', (req, res) => {
+    const faker = require('faker');
+
+    for (i = 0; i < 20; i++) {
+        var user = new User({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.exampleEmail(),
+            birthDate: faker.date.past(),
+            status: faker.lorem.sentence(),
+            pictureUrl: faker.image.imageUrl()
+        });
+        user.password = user.generateHash('hdm');
+
+        user.save((err, data) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('database not responding');
+            }
+        });
+    }
+    res.send('seeded database');
+
+});
+
+router.get('/purge-database', (req, res) => {
+    User.collection.remove((err) => {
+        if (err) {
+            res.send(err);
+        }
+        res.send('all good');
+    });
+});
+
+module.exports = router;
